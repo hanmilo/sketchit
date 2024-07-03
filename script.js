ce193d3b043f0107106a8eb75f53227f756b13dc
hanmilo <hanmilo@gmail.com>
2024-07-03 16:38:50 +0800

    Updated button options & grid generation

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function gridGridGen(pixel_xy) {
+    for (x = 1; x <= pixel_xy; x++) {
+        divGridRow.textContent = "";
+        for (y = 1; y <= pixel_xy; y++) {
+            // divGridBox.textContent = x + ", " + y;
+            divGridRow.appendChild(divGridBox.cloneNode(true));
+            // console.log("Done: divGridBox" + "-" + x + ", " + y);
+        };
+        queryBody.appendChild(divGridRow.cloneNode(true));
+        // console.log("Done: divGridRow");
+    };
+};
+
+function buttonAppend() {
+    queryBody.textContent = "";
+    queryBody.appendChild(buttonNewSketch);
+    queryBody.appendChild(buttonResetSketch);
+};
+
 const queryBody = document.querySelector("body");
 queryBody.classList.add("queryBody");
 
@@ -6,30 +25,35 @@ divGridRow.classList.add("divGridRow");
 divGridRow.style["margin"] = "0px";
 // divGridRow.style["border"] = "1px dotted black";
 
+let pixel_density = 5;
+pixel_density.toString();
+pixel_density += "px";
+
 const divGridBox = document.createElement("div");
 divGridBox.classList.add("divGridBox");
-divGridBox.style["border"] = "2px solid black";
-divGridBox.style["padding"] = "10px";
+divGridBox.style["border"] = "1px dotted gray";
+divGridBox.style["padding"] = pixel_density;
 
-// console.log("Done: Initializing");
+let holdGridPixel = 0;
 
-for (x = 1; x <= 16; x++) {
-    divGridRow.textContent = "";
-    for (y = 1; y <= 16; y++) {
-        // divGridBox.textContent = x + ", " + y;
-        divGridRow.appendChild(divGridBox.cloneNode(true));
-        // console.log("Done: divGridBox" + "-" + x + ", " + y);
-    };
-    queryBody.appendChild(divGridRow.cloneNode(true));
-    // console.log("Done: divGridRow");
-};
+const buttonNewSketch = document.createElement("button");
+buttonNewSketch.classList.add("buttonNewSketch");
+buttonNewSketch.textContent = "New Sketch";
+buttonNewSketch.addEventListener("click", () => {
+    let selectGridPixel = prompt("How many pixels per row & column?", 32);
+    holdGridPixel = selectGridPixel;
 
-divGridBox.addEventListener("click", () => {
-    if (divGridBox.style["backgroundColor"] == "") {
-        divGridBox.style["backgroundColor"] = "lightseagreen";
-    }
-    else {
-        divGridBox.style["backgroundColor"] = "";
-    };
-    console.log("Clicked")
+    buttonAppend();
+    gridGridGen(selectGridPixel);
+});
+
+queryBody.appendChild(buttonNewSketch);
+
+const buttonResetSketch = document.createElement("button");
+buttonResetSketch.classList.add("buttonResetSketch");
+buttonResetSketch.textContent = "Reset Sketch";
+buttonResetSketch.addEventListener("click", () => {
+    
+    buttonAppend();
+    gridGridGen(holdGridPixel);
 });
